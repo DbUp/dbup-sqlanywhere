62b9f1f42c25d1f2b7895515eb97312cd4b9e5b4
Robert Wagner <robert.wagner@octopus.com>
2024-01-24 14:16:33 +1000

    Copy from main project

---
+++ src/Tests/DatabaseSupportTests.cs
@@ -0,0 +1,20 @@
+﻿using DbUp.Builder;
+using DbUp.Tests.Common;
+
+namespace DbUp.SqlAnywhere.Tests;
+
+public class DatabaseSupportTests : DatabaseSupportTestsBase
+{
+    public DatabaseSupportTests() : base()
+    {
+    }
+
+    protected override UpgradeEngineBuilder DeployTo(SupportedDatabases to)
+        => to.SqlAnywhereDatabase("");
+
+    protected override UpgradeEngineBuilder AddCustomNamedJournalToBuilder(UpgradeEngineBuilder builder, string schema, string tableName)
+        => builder.JournalTo(
+            (connectionManagerFactory, logFactory)
+                => new SqlAnywhereTableJournal(connectionManagerFactory, logFactory, schema, tableName)
+        );
+}
---
+++ src/Tests/NoPublicApiChanges.cs
@@ -0,0 +1,11 @@
+﻿using DbUp.Tests.Common;
+
+namespace DbUp.SqlAnywhere.Tests;
+
+public class NoPublicApiChanges : NoPublicApiChangesBase
+{
+    public NoPublicApiChanges()
+        : base(typeof(SqlAnywhereExtensions).Assembly)
+    {
+    }
+}
